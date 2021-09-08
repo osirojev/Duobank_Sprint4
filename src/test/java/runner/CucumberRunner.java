a33850d0d9ba103d325a672c5cde2164f172d554
“Oyat <oyatsirojev@gmail.comgit config --global user.name Oyat Sirojev>
2021-09-07 23:53:58 -0400

    added new step

--- src/test/java/runner/CucumberRunner.java
+++ src/test/java/runner/CucumberRunner.java
@@ -6,7 +6,21 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions( tags = "@temp",   // "@login or @smoke", "@login and @smoke", "@login and not @smoke", "@login and @smoke and @regression"
+        features = "src/test/resources/features", // path where the feature files are located
+//        features = {"src/test/resources/features/ui", "src/test/resources/features/db"}, // for multiple paths
+//        features = "src/test/resources/features/login.feature",  // specific feature file
+        glue = "stepDefinitions" // path where the step Definition methods glued to feature file steps are located
+//        ,dryRun = true    // this is used to generate step definition snippets without actually running the test
+//
+
+        , plugin = {
+        "pretty",   // detailed test run results on the console
+        "html:target/built-in-html-report.html",  // generates built-in html report
+        "json:target/cucumber.json"  // generates a json report that is needed for pretty html report
+},
+        stepNotifications = true,   // shows detailed step results in report
+        snippets = CucumberOptions.SnippetType.CAMELCASE  // generates snippets using camelCase
 
 
 
---
+++ src/test/java/stepDefinitions/SignUpStepDefs.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Assert;
+import pages.LoginPage;
+import pages.SignUpPage;
+import utilities.ConfigReader;
+import utilities.DBUtility;
+import utilities.Driver;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class SignUpStepDefs {
+
+
+
+
+    }
+
